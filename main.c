efaa61dffed476f8fbcb634f689337cd84d35970
EveBaker <evebaker0162.com>
2023-04-03 10:37:22 -0700

    monty

--- main.c
+++ main.c
@@ -3,44 +3,45 @@
 vars var;
 
 /**
-* main - Start, LIFO, FILO program
-* @ac: Number of arguments
-* @av: Pointer containing arguments
-* Return: 0 Success, 1 Failed
-*/
-
+ * main - Start LIFO, FILO program
+ * @ac: Number of arguments
+ * @av: Pointer containing arguments
+ * Return: 0 Success, 1 Failed
+ */
 int main(int ac, char **av)
 {
 	char *opcode;
 
 	if (ac != 2)
 	{
-		fprint(stderr, "USAGE: monty file\n");
-		return (EXIT_FALURE);
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
 	}
 
-	if (start_vars(&var) !=0)
-		return (EXIT_FALURE);
+	if (start_vars(&var) != 0)
+		return (EXIT_FAILURE);
 
-	var.file - fopen(av[1], "r");
+	var.file = fopen(av[1], "r");
 	if (!var.file)
 	{
-		fprint(stderr, "Error: cant open file %s\n", av[i]);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		free_all();
 		return (EXIT_FAILURE);
 	}
 
 	while (getline(&var.buff, &var.tmp, var.file) != EOF)
 	{
-		opcode = strtok(var.buff, "\r\t\n");
+		opcode = strtok(var.buff, " \r\t\n");
 		if (opcode != NULL)
-			if (call_funct(&var, opcode) == EXIT_FALURE)
-		{
-			free_all();
-			return (EXIT_FALURE);
-		}
-	var.line_number++;
+			if (call_funct(&var, opcode) == EXIT_FAILURE)
+			{
+				free_all();
+				return (EXIT_FAILURE);
+			}
+		var.line_number++;
 	}
+
 	free_all();
+
 	return (EXIT_SUCCESS);
 }
